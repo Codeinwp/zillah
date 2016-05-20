59cba8f1b887c8313a892d3ad0f2bf5770f4bdd3
claudiuvertistudio <claudiu.buruiana@codeinwp.com>
2016-05-20 16:54:47 +0300

    #16 Added new design for single post

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -11,44 +11,41 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<?php 
-		if ( ! is_single() ) {
-			if ( has_post_thumbnail() ) {
-				echo '<a href="' . esc_url( get_permalink() ) . '" class="post-thumbnail" rel="bookmark">';
-				the_post_thumbnail();
-				echo '</a>';
-			}
-		}
-	?>
-
 	<header class="entry-header">
-		<?php
-			if ( is_single() ) {
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			} else {
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			}
-		 ?>
-		 <?php 
-		 	echo '<div class="categories-links">' . get_the_category_list( ', ', 'zillah') . '</div>';
-		 ?>
+		<div class="content-inner-wrap">
+			<?php
+			zillah_posted_date();
+			the_title( '<h1 class="entry-title">', '</h1>' );
+			zillah_category();
+			?>
+		</div>
 	</header><!-- .entry-header -->
 
+	<?php
+	if ( has_post_thumbnail() ) {
+		echo '<a href="' . esc_url( get_permalink() ) . '" class="post-thumbnail" rel="bookmark">';
+		the_post_thumbnail( 'post-thumbnail' );
+		echo '</a>';
+	}
+	?>
+
 	<div class="entry-content">
-		<?php
+		<div class="content-inner-wrap">
+			<?php
 
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'zillah' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
+				the_content( sprintf(
+					/* translators: %s: Name of current post. */
+					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'zillah' ), array( 'span' => array( 'class' => array() ) ) ),
+					the_title( '<span class="screen-reader-text">"', '"</span>', false )
+				) );
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'zillah' ),
-				'after'  => '</div>',
-			) );
-			
-		?>
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'zillah' ),
+					'after'  => '</div>',
+				) );
+
+			?>
+		</div>
 	</div><!-- .entry-content -->
 
 </article><!-- #post-## -->
