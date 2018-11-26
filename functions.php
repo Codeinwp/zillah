021acfb926d46b613ee6f9ece54d937d2a743244
coti <stefan@themeisle.com>
2018-11-26 15:27:47 +0200

    Add notice to Neve in Dashboard

--- functions.php
+++ functions.php
@@ -970,3 +970,46 @@ function zillah_post_video() {
 	}
 
 }
+
+/**
+ * Add a dismissible notice about Neve in the dashboard
+ */
+function zillah_neve_notice() {
+	global $current_user;
+	$user_id        = $current_user->ID;
+	$ignored_notice = get_user_meta( $user_id, 'zillah_ignore_neve_notice' );
+	if ( ! empty( $ignored_notice ) ) {
+		return;
+	}
+	$dismiss_button =
+		sprintf(
+			/* translators: Install Neve link */
+			'<a href="%s" class="notice-dismiss" style="text-decoration:none;"></a>',
+			'?zillah_nag_ignore_neve=0'
+		);
+	$message = sprintf(
+		/* translators: Install Neve link */
+		esc_html__( 'Check out %1$s. Fully AMP optimized and responsive, Neve will load in mere seconds and adapt perfectly on any viewing device. Neve works perfectly with Gutenberg and the most popular page builders. You will love it!', 'zillah' ),
+		sprintf(
+			/* translators: Install Neve link */
+			'<a target="_blank" href="%1$s"><strong>%2$s</strong></a>',
+			esc_url( admin_url( 'theme-install.php?theme=neve' ) ),
+			esc_html__( 'our newest theme', 'zillah' )
+		)
+	);
+	printf( '<div class="notice updated" style="position:relative; padding-right: 35px;">%1$s<p>%2$s</p></div>', $dismiss_button, $message );
+}
+add_action( 'admin_notices', 'zillah_neve_notice' );
+
+/**
+ * Update the zillah_ignore_neve_notice option to true, to dismiss the notice from the dashboard
+ */
+function zillah_nag_ignore_neve() {
+	global $current_user;
+	$user_id = $current_user->ID;
+	/* If user clicks to ignore the notice, add that to their user meta */
+	if ( isset( $_GET['zillah_nag_ignore_neve'] ) && '0' == $_GET['zillah_nag_ignore_neve'] ) {
+		add_user_meta( $user_id, 'zillah_ignore_neve_notice', 'true', true );
+	}
+}
+add_action( 'admin_init', 'zillah_nag_ignore_neve' );
